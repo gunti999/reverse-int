c8e0c95570abf43501c5da2b7500d55f748104bf
Ilya Ivchenkov <gunti911@gmail.com>
2022-01-31 14:55:01 +0300

    init: reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    let abs = Math.abs(n);
+    let arr = String(abs).split('');
+    let arrReverse = arr.reverse();
+    return Number(arrReverse.join(''));
 }
